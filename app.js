c85abebec444fd6ee71211aed41940ea0a8da350
serhatbek <serhat.bekk@gmail.com>
2022-02-17 01:29:02 +0300

    add readme file

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const burgerBtn = document.querySelector('.burgerMenu');
 const navOpen = document.querySelector('.nav');
 const header = document.querySelector('header');
 
+// Toggle navbar
 burgerBtn.addEventListener('click', () => {
   burgerBtn.classList.toggle('active');
   navOpen.classList.toggle('open');
